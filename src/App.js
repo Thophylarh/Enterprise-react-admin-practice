02727a08157376ad626bf314aeec12018234aba3
Thophylarh <iwualathophylarh@gmail.com>
2023-09-28 09:54:22 +0100

    tobar ui

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 
-
+import Topbar from "./scenes/dashboard/global/Topbar";
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 
 function App() {
+  const [theme, colorMode] = useMode();
   return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline/>
     <div className="App">
-    
+      <main className="content">
+        <Topbar/>
+      </main>
     </div>
+    </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
