2a3476c40f6873b712b4a4b4113c4d8806451976
Thophylarh <iwualathophylarh@gmail.com>
2023-09-28 15:51:06 +0100

    team component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Dashboard from "./scenes/dashboard/index";
 
 import Sidebar from "./scenes/dashboard/global/Sidebar";
 import { useState } from "react";
+import Team from "./scenes/dashboard/team";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -15,11 +16,12 @@ function App() {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="App">
-        <Sidebar isSidebar={isSidebar} />
+          <Sidebar isSidebar={isSidebar} />
           <main className="content">
             <Topbar setIsSidebar={setIsSidebar} />
             <Routes>
               <Route path="/" element={<Dashboard />} />
+              <Route path="/team" element={<Team />} />
             </Routes>
           </main>
         </div>
